47318a4e7e5c9d6500a4be41e1140852f7e89412
Mark Hermsdorfer <m.hermsdorfer@f5.com>
2023-05-23 13:11:50 -0400

    removed iam role, fixed ec2 ssh key on instance.

--- main.tf
+++ main.tf
@@ -31,112 +31,112 @@ resource "aws_key_pair" "deployer" {
 }
 
 # Create IAM policy
-resource "aws_iam_policy" "f5xc_iam_policy" {
-  name = "f5xc_iam_policy_${random_id.buildSuffix.hex}"
-  path = "/"
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-        {
-            "Action" = [
-                "autoscaling:DescribeAutoScalingGroups",
-                "autoscaling:DescribeAutoScalingInstances",
-                "autoscaling:DescribeLaunchConfigurations",
-                "autoscaling:DescribeTags",
-                "autoscaling:SetDesiredCapacity",
-                "autoscaling:TerminateInstanceInAutoScalingGroup",
-                "ec2:DescribeInstances",
-                "ec2:DescribeRegions",
-                "ec2:DescribeRouteTables",
-                "ec2:DescribeSecurityGroups",
-                "ec2:DescribeSubnets",
-                "ec2:DescribeVolumes",
-                "ec2:CreateSecurityGroup",
-                "ec2:CreateTags",
-                "ec2:CreateVolume",
-                "ec2:ModifyInstanceAttribute",
-                "ec2:ModifyVolume",
-                "ec2:AttachVolume",
-                "ec2:AuthorizeSecurityGroupIngress",
-                "ec2:CreateRoute",
-                "ec2:DeleteRoute",
-                "ec2:DeleteSecurityGroup",
-                "ec2:DeleteVolume",
-                "ec2:DetachVolume",
-                "ec2:RevokeSecurityGroupIngress",
-                "ec2:DescribeVpcs",
-                "elasticloadbalancing:AddTags",
-                "elasticloadbalancing:AttachLoadBalancerToSubnets",
-                "elasticloadbalancing:ApplySecurityGroupsToLoadBalancer",
-                "elasticloadbalancing:CreateLoadBalancer",
-                "elasticloadbalancing:CreateLoadBalancerPolicy",
-                "elasticloadbalancing:CreateLoadBalancerListeners",
-                "elasticloadbalancing:ConfigureHealthCheck",
-                "elasticloadbalancing:DeleteLoadBalancer",
-                "elasticloadbalancing:DeleteLoadBalancerListeners",
-                "elasticloadbalancing:DescribeLoadBalancers",
-                "elasticloadbalancing:DescribeLoadBalancerAttributes",
-                "elasticloadbalancing:DetachLoadBalancerFromSubnets",
-                "elasticloadbalancing:DeregisterInstancesFromLoadBalancer",
-                "elasticloadbalancing:ModifyLoadBalancerAttributes",
-                "elasticloadbalancing:RegisterInstancesWithLoadBalancer",
-                "elasticloadbalancing:SetLoadBalancerPoliciesForBackendServer",
-                "elasticloadbalancing:AddTags",
-                "elasticloadbalancing:CreateListener",
-                "elasticloadbalancing:CreateTargetGroup",
-                "elasticloadbalancing:DeleteListener",
-                "elasticloadbalancing:DeleteTargetGroup",
-                "elasticloadbalancing:DescribeListeners",
-                "elasticloadbalancing:DescribeLoadBalancerPolicies",
-                "elasticloadbalancing:DescribeTargetGroups",
-                "elasticloadbalancing:DescribeTargetHealth",
-                "elasticloadbalancing:ModifyListener",
-                "elasticloadbalancing:ModifyTargetGroup",
-                "elasticloadbalancing:RegisterTargets",
-                "elasticloadbalancing:SetLoadBalancerPoliciesOfListener",
-                "iam:CreateServiceLinkedRole",
-                "kms:DescribeKey"
-            ],
-            "Effect": "Allow",
-            "Resource": [
-                "*"
-            ]
-        }
-    ]
-
-  })
-}
+#resource "aws_iam_policy" "f5xc_iam_policy" {
+#  name = "f5xc_iam_policy_${random_id.buildSuffix.hex}"
+#  path = "/"
+#  policy = jsonencode({
+#    Version = "2012-10-17"
+#    Statement = [
+#        {
+#            "Action" = [
+#                "autoscaling:DescribeAutoScalingGroups",
+#                "autoscaling:DescribeAutoScalingInstances",
+#                "autoscaling:DescribeLaunchConfigurations",
+#                "autoscaling:DescribeTags",
+#                "autoscaling:SetDesiredCapacity",
+#                "autoscaling:TerminateInstanceInAutoScalingGroup",
+#                "ec2:DescribeInstances",
+#                "ec2:DescribeRegions",
+#                "ec2:DescribeRouteTables",
+#                "ec2:DescribeSecurityGroups",
+#                "ec2:DescribeSubnets",
+#                "ec2:DescribeVolumes",
+#                "ec2:CreateSecurityGroup",
+#                "ec2:CreateTags",
+#                "ec2:CreateVolume",
+#                "ec2:ModifyInstanceAttribute",
+#                "ec2:ModifyVolume",
+#                "ec2:AttachVolume",
+#                "ec2:AuthorizeSecurityGroupIngress",
+#                "ec2:CreateRoute",
+#                "ec2:DeleteRoute",
+#                "ec2:DeleteSecurityGroup",
+#                "ec2:DeleteVolume",
+#                "ec2:DetachVolume",
+#                "ec2:RevokeSecurityGroupIngress",
+#                "ec2:DescribeVpcs",
+#                "elasticloadbalancing:AddTags",
+#                "elasticloadbalancing:AttachLoadBalancerToSubnets",
+#                "elasticloadbalancing:ApplySecurityGroupsToLoadBalancer",
+#                "elasticloadbalancing:CreateLoadBalancer",
+#                "elasticloadbalancing:CreateLoadBalancerPolicy",
+#                "elasticloadbalancing:CreateLoadBalancerListeners",
+#                "elasticloadbalancing:ConfigureHealthCheck",
+#                "elasticloadbalancing:DeleteLoadBalancer",
+#                "elasticloadbalancing:DeleteLoadBalancerListeners",
+#                "elasticloadbalancing:DescribeLoadBalancers",
+#                "elasticloadbalancing:DescribeLoadBalancerAttributes",
+#                "elasticloadbalancing:DetachLoadBalancerFromSubnets",
+#                "elasticloadbalancing:DeregisterInstancesFromLoadBalancer",
+#                "elasticloadbalancing:ModifyLoadBalancerAttributes",
+#                "elasticloadbalancing:RegisterInstancesWithLoadBalancer",
+#                "elasticloadbalancing:SetLoadBalancerPoliciesForBackendServer",
+#                "elasticloadbalancing:AddTags",
+#                "elasticloadbalancing:CreateListener",
+#                "elasticloadbalancing:CreateTargetGroup",
+#                "elasticloadbalancing:DeleteListener",
+#                "elasticloadbalancing:DeleteTargetGroup",
+#                "elasticloadbalancing:DescribeListeners",
+#                "elasticloadbalancing:DescribeLoadBalancerPolicies",
+#                "elasticloadbalancing:DescribeTargetGroups",
+#                "elasticloadbalancing:DescribeTargetHealth",
+#                "elasticloadbalancing:ModifyListener",
+#                "elasticloadbalancing:ModifyTargetGroup",
+#                "elasticloadbalancing:RegisterTargets",
+#                "elasticloadbalancing:SetLoadBalancerPoliciesOfListener",
+#                "iam:CreateServiceLinkedRole",
+#                "kms:DescribeKey"
+#            ],
+#            "Effect": "Allow",
+#            "Resource": [
+#                "*"
+#            ]
+#        }
+#    ]
+#
+#  })
+#}
 
 # Create a role
-resource "aws_iam_role" "f5xc_role" {
-  name = "f5xc_role_${random_id.buildSuffix.hex}"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    "Statement" = [
-        {
-            "Sid" = "",
-            "Effect" = "Allow",
-            "Principal" = {
-                "Service" = "ec2.amazonaws.com"
-            },
-            "Action" = "sts:AssumeRole"
-        }
-    ]
-  })
-}
+#resource "aws_iam_role" "f5xc_role" {
+#  name = "f5xc_role_${random_id.buildSuffix.hex}"
+#  assume_role_policy = jsonencode({
+#    Version = "2012-10-17"
+#    "Statement" = [
+#        {
+#            "Sid" = "",
+#            "Effect" = "Allow",
+#            "Principal" = {
+#                "Service" = "ec2.amazonaws.com"
+#            },
+#            "Action" = "sts:AssumeRole"
+#        }
+#    ]
+#  })
+#}
 
 # Attach role to policy
-resource "aws_iam_policy_attachment" "f5xc_policy_role" {
-  name = "f5xc_policy_role_${random_id.buildSuffix.hex}"
-  roles = [aws_iam_role.f5xc_role.name]
-  policy_arn = aws_iam_policy.f5xc_iam_policy.arn
-}
+#resource "aws_iam_policy_attachment" "f5xc_policy_role" {
+#  name = "f5xc_policy_role_${random_id.buildSuffix.hex}"
+#  roles = [aws_iam_role.f5xc_role.name]
+#  policy_arn = aws_iam_policy.f5xc_iam_policy.arn
+#}
 
 # Attach role to an instance profile
-resource "aws_iam_instance_profile" "f5xc_profile" {
-  name = "f5xc_profile_${random_id.buildSuffix.hex}"
-  role = aws_iam_role.f5xc_role.name
-}
+#resource "aws_iam_instance_profile" "f5xc_profile" {
+#  name = "f5xc_profile_${random_id.buildSuffix.hex}"
+#  role = aws_iam_role.f5xc_role.name
+#}
 
 
 
@@ -269,6 +269,7 @@ resource "aws_instance" "f5xc_ce1" {
   get_password_data = false
   monitoring        = false
   availability_zone = var.az1
+  key_name = "aws_key_pair.deployer.key_name"
 
   user_data_replace_on_change = true
   user_data = templatefile("${path.module}/cloud_init.yaml.template",
@@ -309,6 +310,7 @@ resource "aws_instance" "f5xc_ce2" {
   get_password_data = false
   monitoring        = false
   availability_zone = var.az2
+  key_name = "${tls_private_key.newkey.private_key_pem}"
 
   user_data_replace_on_change = true
   user_data = templatefile("${path.module}/cloud_init.yaml.template",
@@ -349,6 +351,7 @@ resource "aws_instance" "f5xc_ce3" {
   get_password_data = false
   monitoring        = false
   availability_zone = var.az3
+  key_name = "${tls_private_key.newkey.private_key_pem}"
 
   user_data_replace_on_change = true
   user_data = templatefile("${path.module}/cloud_init.yaml.template",
